f6787b128884156bcfc1bc731199e910e352a4f1
joerandy <33285293+joerandy@users.noreply.github.com>
2018-10-15 09:32:01 +0200

    Removing files from x64/Debug
    Håber det løser problemet med path's

--- OpenCVTest/DatabaseIO.h
+++ OpenCVTest/DatabaseIO.h
@@ -1,9 +1,26 @@
 #pragma once
 class DatabaseIO
 {
+#include <stdlib.h>
+#include <iostream>
+#include "pch.h"
+
+	/*
+	  Include directly the different
+	  headers from cppconn/ and mysql_driver.h + mysql_util.h
+	  (and mysql_connection.h). This will reduce your build time!
+	*/
+#include <jdbc/mysql_connection.h>
+#include <jdbc/mysql_driver.h>
+#include <jdbc/cppconn/exception.h>
+#include <jdbc/cppconn/resultset.h>
+#include <jdbc/cppconn/statement.h>
+
+	using namespace std;
 
 public:
 	DatabaseIO();
 	~DatabaseIO();
+
 };
 
